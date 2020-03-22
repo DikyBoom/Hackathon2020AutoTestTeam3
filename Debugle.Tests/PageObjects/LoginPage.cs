aa7e1c7d9c9d5457dcc3d0c110a633528db9b930
DikyBoom <DikyBoom@users.noreply.github.com>
2020-03-22 17:47:01 +0200

    Upload Project

---
+++ Debugle.Tests/PageObjects/LoginPage.cs
@@ -0,0 +1,58 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+namespace Debugle.Tests
+{
+    public class LoginPage
+    {
+        private IWebDriver _driver;
+        By _userLoginLocator = By.XPath(".//*[@class='input-text__input input-text__input_size-regular']");
+        By _userPasswordLocator = By.Id("id-p");
+        By _loginButtonLocator = By.XPath(".//*[@type='submit']");
+        By _errorMessageDivLocator = By.ClassName("form__error form__error_fail");
+
+        public LoginPage(IWebDriver driver)
+        {
+            _driver = driver;
+       }
+
+        public void TypeUserLogin(string userLogin)
+        {
+            Thread.Sleep(1000);
+            _driver.FindElement(_userLoginLocator).SendKeys(userLogin);
+
+        }
+
+        public void TypeUserPassword(string userPassword)
+        {
+            _driver.FindElement(_userPasswordLocator).SendKeys(userPassword);
+        }
+
+        public void ClickLoginButton()
+        {
+            _driver.FindElement(_loginButtonLocator).Click();
+        }
+
+        public string GiveErrorMessageText()
+        {
+            return _driver.FindElement(_errorMessageDivLocator).Text;
+        }
+
+        ////public string GiveEmailFieldValidationMesssageText()
+        ////{
+        ////    return _driver.FindElement(_userLoginLocator).GetAttribute("validationMessage");
+        ////}
+
+        ////public string GivePasswordFieldValidationMesssageText()
+        ////{
+        ////    return _driver.FindElement(_userPasswordLocator).GetAttribute("validationMessage");
+        ////}
+        
+
+    }
+
+}
---
+++ Debugle.Tests/PageObjects/MailPage.cs
@@ -0,0 +1,62 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+
+namespace Debugle.Tests
+{
+    public class MailPage
+    {
+        private IWebDriver _driver;
+        By _clicCreatMail = By.XPath("//button[@class='default compose']");
+        By _clicAdress = By.XPath("//div[@class='sendmsg__form-label-field auto cropped ui-sortable']");
+        //By _clicTopic = By.XPath("//input[@class = 'input']");
+        By _clicBodyMail = By.XPath("//body[@id='tinymce']']");
+        By _clicButtonSend = By.XPath("//button[@class='default send']");
+        By _clicButtonPodtverjdenie = By.XPath("//*[text()='Все равно отправить']");
+        By _numberOfLetters = By.XPath("//span[@class='sidebar__list-link-count']");
+        int NumbersLetterBase;
+        public MailPage(IWebDriver driver)
+        {
+            _driver = driver;
+            
+        }
+        public void ButtonCreatMail()
+        {
+            _driver.FindElement(_clicCreatMail).Click();
+        }
+       public void ClicFieldSend()
+        {
+            _driver.FindElement(_clicAdress).Click();
+            Thread.Sleep(1000);
+            
+        }
+
+        public void WriteMyMail(string mail)
+        {
+            IWebElement rte = _driver.SwitchTo().ActiveElement();
+            rte.SendKeys(mail);
+            _driver.SwitchTo().DefaultContent();
+            Thread.Sleep(1000);
+         }
+       
+        public void ClicSendMail()
+        {
+            _driver.FindElement(_clicButtonSend).Click();
+            Thread.Sleep(1000);
+        }
+        public void ClicPodtverjdenie()
+        {
+            _driver.FindElement(_clicButtonPodtverjdenie).Click();
+            Thread.Sleep(1000);
+        }
+        public string NumbersLetter()
+        {
+            return _driver.FindElement(_numberOfLetters).Text;
+        }
+       
+    }
+}
